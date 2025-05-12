2756999d9780ec52d37261a9435409c80bd73b49
itsdragzz <kelvinliang123@icloud.com>
2025-05-12 19:38:34 +1000

    Create Header.jsx

---
+++ src/Components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+function Header() {
+  return (
+    <div className={styles.footer}>
+      <p>
+        Header
+      </p>
+
+    </div>
+  )
+}
+
+export default Header
